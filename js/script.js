bd8cdfdbf382ec5194df7b8471fd54a1b44e191f
Duran914 <jonathan.duran.100@my.csun.edu>
2018-05-24 23:35:29 -0700

    added apply form modal

--- js/script.js
+++ js/script.js
@@ -14,17 +14,23 @@ const videoModalBtn = document.querySelector('#video-modal'); // video play butt
 
 const videoModal = document.querySelector('.modal'); // video modal
 
-const foodVideo = document.querySelector('.food-video');// video
+// const foodVideo = document.querySelector('.food-video');// video
 
 
 // Window functions
-window.onscroll = function() {stickyNavbar()};
-window.onclick = function(){closeModal()};
+window.onscroll = function(){stickyNavbar()};
+
+window.onclick = function()
+    { 
+      closeModal(), 
+      closeFormModal() 
+    };
 
 
 // Event listeners 
-modalCloseX.addEventListener('click', closeModalByX);
-videoModalBtn.addEventListener('click', openVidoeModal);
+
+// modalCloseX.addEventListener('click', closeModalByX);
+videoModalBtn.addEventListener('click', openVideoModal);
 
 
 // Create sticky navbar when reaches navigation bar
@@ -58,14 +64,22 @@ $(document).ready(function(){
     }
   });
 });
-// open modals
-function openVidoeModal() {  
+
+// open video modal
+function openVideoModal() {  
   videoModal.style.display = "block";
+  videoModal.innerHTML = `
+  <div class="video-modal-content">
+  <span id="close-video-modal" onclick=closeModalByX() class="close">&times;</span>
+  <video class="food-video" src="assets/videos/food-video.mp4" controls></video>
+</div>`;
+  
 }
 
 // closes modal by clicking outside video-modal
 function closeModal() {
   if (event.target == videoModal) {
+    const foodVideo = document.querySelector('.food-video');
     videoModal.style.display = "none";
     foodVideo.pause();
   }
@@ -73,7 +87,81 @@ function closeModal() {
 
 // closes video modal by click on the X
 function closeModalByX() {
+  const foodVideo = document.querySelector('.food-video');
     videoModal.style.display = "none";
     foodVideo.pause();
 }
 
+const menuItem = document.querySelector('#menu-item');
+
+menuItem.addEventListener('click', addMenuItem)
+
+function addMenuItem() {
+    let alert = document.createElement('span');
+    alert.innerHTML = "Item added";
+    let test = document.querySelector('.breakfest');
+    let parant = test.firstChild;
+    test.insertBefore(alert, parant)
+}
+
+
+
+const applyBtn = document.querySelector('#joinUsBtn');
+
+const applyModal = document.querySelector('.modal-form');
+
+const closeFormModalEvent = document.querySelector('#close-form-modal');
+
+// closeFormModalEvent.addEventListener('click', closeFormModalByX)
+applyBtn.addEventListener('click', applyWithUs);
+
+// opens modal
+
+function applyWithUs(){
+   applyModal.style.display = "block";
+  // let form = document.createElement('div');
+  applyModal.innerHTML = `<div class="form-modal-content">
+  <span id="close-form-modal" onclick=closeFormModalByX() class="close">&times;</span>
+              <form>
+                   <div class="form-group">
+                        <label for="first-name">First Name</label>
+                        <input type="text" class="form-control" id="first-name" placeholder="First name">
+                      </div>
+                      <div class="form-group">
+                        <label for="last-name">Last Name</label>
+                        <input type="text" class="form-control" id="last-name" placeholder="Last name">
+                      </div>
+                      <div class="form-group">
+                              <label for="category">Category</label>
+                              <select class="form-control" id="exampleFormControlSelect1">
+                                <option value="Choose a Category">Choose a Category</option>
+                                <option>Accounting</option>
+                                <option>Hospitality</option>
+                                <option>Information Technology</option>
+                                <option>Marketing</option>
+                                <option>Human Resources</option>
+                              </select>
+                            </div>
+                            <div class="form-group">
+                                  <label for="upload">Upload a Resume</label>
+                                  <input type="file" class="form-control-file" id="file-upload">
+                           </div>
+                           <div class="form-group">
+                                  <label for="cover-letter">Cover Letter</label>
+                                  <textarea class="form-control message-field" id="messageField" rows="5"></textarea>
+                                </div>
+                      <button type="submit" class="btn applyFormBtn">Submit</button>
+                    </form>
+              </div>`
+}
+//closes modal by X
+function closeFormModalByX() {
+  applyModal.style.display = "none";
+  // applyModal.remove();
+}
+//closes modal by click outside modal
+function closeFormModal() {
+  if (event.target == applyModal) {
+    applyModal.style.display = "none";
+  }
+}
